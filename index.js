d7cb3552defea0207eea2e2766794406d82fdf3e
asynchronous-widget-3412 <rikeiwilliams@gmail.com>
2019-07-26 15:02:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(array) {
-  let kitty = ["Ralph"];
-  return array.concat(kitty);
+function destructivelyAppendKitten(array, kitten) {
+  return array.push(kitten);
+ 
 
 }
 
