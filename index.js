3722c514a487856a6488ebc85ee8d48b2a669012
asynchronous-widget-3412 <rikeiwilliams@gmail.com>
2019-07-26 15:07:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ function destructivelyAppendKitten(kitten) {
   return kittens.push(kitten);
  }
 
-function destuctivelyPrependKitten
+function destuctivelyPrependKitten(kitten) {
+  retrun kittens.shift(kitten)
+}
